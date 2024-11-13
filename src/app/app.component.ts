f6cd16dee8ae16a99937f2de95e32f280d31f3b9
kovalenko_evgeniy <kovalenko@a2design.biz>
2024-11-13 12:37:06 +0600

    add calc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CalculatorComponent} from '../calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-tutor';
+  public title = 'Мой калькулятор';
+
+  public tooltip = "Подсказка"
+
+  protected showMessage(){
+    alert('hello')
+  }
+
 }
